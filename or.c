3abe0794b1f1bca29075366bfd90d9dc5d9e2a2e
30618 <129346699+duoll0323@users.noreply.github.com>
2023-04-04 01:10:39 +0000

    ㄴ

--- or.c
+++ or.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-main() {
-  int a = 15;
-  int b = 20;
-  int c = a | b;
+// main() {
+//   int a = 15;
+//   int b = 20;
+//   int c = a | b;
 
-  printf("OR 연산 결과 : %d", c);
-}
+//   printf("OR 연산 결과 : %d", c);
+// }
